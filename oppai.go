91b1aacdc76961e616a1d3d8fa880a0351f40420
mattn <mattn.jp@gmail.com>
2011-11-28 13:27:44 +0900

    gofmt

--- oppai.go
+++ oppai.go
@@ -1,73 +1,76 @@
 package oppai
 
 type Oppai struct {
-	count int;
-	cmd []map[string]string
+	count int
+	cmd   []map[string]string
 }
 
 func (self *Oppai) Massage() *Oppai {
-	self.cmd = append(self.cmd, map[string] string { "name": "Massage", "message": "" });
-	return self;
+	self.cmd = append(self.cmd, map[string]string{"name": "Massage", "message": ""})
+	return self
 }
 
 func (self *Oppai) Oppai(msg string) *Oppai {
-	self.cmd = append(self.cmd, map[string] string { "name": "Oppai", "message": msg } );
-	return self;
+	self.cmd = append(self.cmd, map[string]string{"name": "Oppai", "message": msg})
+	return self
 }
 
 func (self *Oppai) Done() (buf string) {
-	var c int = 0;
+	var c int = 0
 
 	for i := 0; i < len(self.cmd); i++ {
-		var cmd string = self.cmd[i]["name"];
-		var msg string = self.cmd[i]["message"];
+		var cmd string = self.cmd[i]["name"]
+		var msg string = self.cmd[i]["message"]
 
 		if cmd == "Massage" {
-			buf += doneMassage();
-			c = 0;
+			buf += doneMassage()
+			c = 0
 		} else if cmd == "Oppai" {
-			if c % 2 == 0 {
-				buf += doneOppaiUp(msg);
+			if c%2 == 0 {
+				buf += doneOppaiUp(msg)
 			} else {
-				buf += doneOppaiDown(msg);
+				buf += doneOppaiDown(msg)
 			}
-			c++;
+			c++
 		}
 	}
 
 	self.cmd = make([]map[string]string, 0)
-	return buf;
+	return buf
 }
 
 func doneMassage() (aa string) {
-	aa  = "　　　 _ 　∩\n";
-	aa += "　　(　゜∀゜)彡　おっぱい!おっぱい!\n";
-	aa += "　　(　 ⊂彡\n";
-	aa += "　 　|　　　|　\n";
-	aa += "　 　し ⌒Ｊ\n";	
-	return aa;
+	aa = "　　　 _ 　∩\n"
+	aa += "　　(　゜∀゜)彡　おっぱい!おっぱい!\n"
+	aa += "　　(　 ⊂彡\n"
+	aa += "　 　|　　　|　\n"
+	aa += "　 　し ⌒Ｊ\n"
+	return aa
 }
 
 func doneOppaiUp(msg string) (aa string) {
-	if msg == "" { msg = "おっぱい!" }
+	if msg == "" {
+		msg = "おっぱい!"
+	}
 
-	aa  = "　　　 _ 　∩\n";
-	aa += "　　(　゜∀゜)彡　" + msg + "\n";
-	aa += "　　(　 　　|　\n";
-	aa += "　 　|　　　|　\n";
-	aa += "　 　し ⌒Ｊ\n";
+	aa = "　　　 _ 　∩\n"
+	aa += "　　(　゜∀゜)彡　" + msg + "\n"
+	aa += "　　(　 　　|　\n"
+	aa += "　 　|　　　|　\n"
+	aa += "　 　し ⌒Ｊ\n"
 
-	return aa;
+	return aa
 }
 func doneOppaiDown(msg string) (aa string) {
-	if msg == "" { msg = "おっぱい!" }
+	if msg == "" {
+		msg = "おっぱい!"
+	}
 
-	aa  = "　　　 _ 　\n";
-	aa += "　　(　゜∀゜)　　" + msg + "\n";
-	aa += "　　(　 ⊂彡\n";
-	aa += "　 　|　　　|　\n";
-	aa += "　 　し ⌒Ｊ\n";
+	aa = "　　　 _ 　\n"
+	aa += "　　(　゜∀゜)　　" + msg + "\n"
+	aa += "　　(　 ⊂彡\n"
+	aa += "　 　|　　　|　\n"
+	aa += "　 　し ⌒Ｊ\n"
 
-	return aa;
+	return aa
 }
-
